b1988c3cade9c724bd308180c2b69d29cc6908a1
maciejbaba <maciek2baba2@gmail.com>
2024-05-04 21:08:26 +0200

    enable prettier??

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -85,6 +85,7 @@ lspconfig.pyright.setup {}
 lspconfig.tsserver.setup {}
 lspconfig.phpactor.setup {}
 lspconfig.gopls.setup {}
+lspconfig.html.setup {}
 lspconfig.powershell_es.setup{
   filetypes = {"ps1", "psm1", "psd1"},
   bundle_path = "/powershell_es",
@@ -97,6 +98,60 @@ lspconfig.rust_analyzer.setup {
   },
 }
 
+-- for prettier to work
+local null_ls = require("null-ls")
+
+local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+local event = "BufWritePre" -- or "BufWritePost"
+local async = event == "BufWritePost"
+
+null_ls.setup({
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.keymap.set("n", "<Leader>f", function()
+        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+      end, { buffer = bufnr, desc = "[lsp] format" })
+
+      -- format on save
+      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+      vim.api.nvim_create_autocmd(event, {
+        buffer = bufnr,
+        group = group,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr, async = async })
+        end,
+        desc = "[lsp] format on save",
+      })
+    end
+
+    if client.supports_method("textDocument/rangeFormatting") then
+      vim.keymap.set("x", "<Leader>f", function()
+        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+      end, { buffer = bufnr, desc = "[lsp] format" })
+    end
+  end,
+})
+
+
+local prettier = require("prettier")
+
+prettier.setup({
+  bin = 'prettier', -- or `'prettierd'` (v0.23.3+)
+  filetypes = {
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "less",
+    "markdown",
+    "scss",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+  },
+})
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
